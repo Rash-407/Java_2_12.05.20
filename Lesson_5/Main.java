291d3517bddb15677f8136a05ff75ad046c72ef3
Rash407 <04070@mail.ru>
2020-05-29 15:22:11 +0500

    Сделал ДЗ к 5-у уроку

--- Lesson_5/Main.java
+++ Lesson_5/Main.java
@@ -4,18 +4,30 @@ public class Main extends Thread {
 
     static final int size = 10000000;
     static final int h = size / 2;
+    private static float[] arr1 = new float[size];
+    private static float[] arr2 = new float[size];
+    private static boolean equals = true;
 
     public static void main (String[] args) {
         oneThread();
         twoThreads();
+        for (int i = 0; i < arr1.length; i++) {
+            if (arr1[i] != arr2[i]) {
+                System.out.println(i+": "+arr1[i]+" - "+arr2[i]);
+                equals=false;
+                return;
+            }
+        }
+        System.out.println(equals);
     }
 
     static private void oneThread () {
         float[] arr = new float[size];
         fillArray(arr, 1);
         long a = System.currentTimeMillis();
-        work(arr);
+        work(arr,0);
         System.out.println(System.currentTimeMillis() - a);
+        arr1 = arr;
     }
 
     static private void twoThreads () {
@@ -23,8 +35,9 @@ public class Main extends Thread {
         fillArray(arr, 1);
         float[] a1 = new float[h];
         float[] a2 = new float[h];
-        Thread t1 = new Thread(() -> work(a1));
-        Thread t2 = new Thread(() -> work(a2));
+        Thread t1 = new Thread(() -> work(a1,0));
+        Thread t2 = new Thread(() -> work(a2,h));
+
 
         long a = System.currentTimeMillis();
         System.arraycopy(arr, 0, a1, 0, h);
@@ -40,6 +53,7 @@ public class Main extends Thread {
         System.arraycopy(a1, 0, arr, 0, h);
         System.arraycopy(a2, 0, arr, h, h);
         System.out.println(System.currentTimeMillis() - a);
+        arr2 = arr;
     }
 
     static private void fillArray (float[] array, int number) {
@@ -47,7 +61,7 @@ public class Main extends Thread {
             array[i]= number;
         }
     }
-    static private void work (float[] arr) {
-        for (int i = 0; i < arr.length; i++) arr[i] = (float)(arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+    static private void work (float[] arr, int pos) {
+        for (int i = 0; i < arr.length; i++) arr[i] = (float)(arr[i] * Math.sin(0.2f + (i+pos) / 5) * Math.cos(0.2f + (i+pos) / 5) * Math.cos(0.4f + (i+pos) / 2));
     }
 }
